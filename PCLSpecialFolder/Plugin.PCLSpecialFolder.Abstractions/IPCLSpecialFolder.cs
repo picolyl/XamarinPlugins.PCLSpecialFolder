af17a314df56715ef506a72535718042774d8206
picolyl <picolyl@gmail.com>
2016-04-24 01:47:53 +0900

    Add interface

--- PCLSpecialFolder/Plugin.PCLSpecialFolder.Abstractions/IPCLSpecialFolder.cs
+++ PCLSpecialFolder/Plugin.PCLSpecialFolder.Abstractions/IPCLSpecialFolder.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PCLStorage;
+using System;
 
 namespace Plugin.PCLSpecialFolder.Abstractions
 {
@@ -7,5 +8,41 @@ namespace Plugin.PCLSpecialFolder.Abstractions
     /// </summary>
     public interface IPCLSpecialFolder
     {
+        /// <summary>
+        /// Application folder
+        /// </summary>
+        IFolder App { get; }
+        /// <summary>
+        /// Local data folder
+        /// </summary>
+        IFolder Local { get; }
+        /// <summary>
+        /// Roaming data folder
+        /// </summary>
+        IFolder Roaming { get; }
+        /// <summary>
+        /// Tamporary data folder
+        /// </summary>
+        IFolder Temporary { get; }
+        /// <summary>
+        /// Cache folder
+        /// </summary>
+        IFolder Cache { get; }
+        /// <summary>
+        /// Documents folder
+        /// </summary>
+        IFolder Documents { get; }
+        /// <summary>
+        /// Pictures folder
+        /// </summary>
+        IFolder Pictures { get; }
+        /// <summary>
+        /// Music folder
+        /// </summary>
+        IFolder Music { get; }
+        /// <summary>
+        /// Videos folder
+        /// </summary>
+        IFolder Videos { get; }
     }
 }
--- PCLSpecialFolder/Plugin.PCLSpecialFolder.Desktop/PCLSpecialFolderImplementation.cs
+++ PCLSpecialFolder/Plugin.PCLSpecialFolder.Desktop/PCLSpecialFolderImplementation.cs
@@ -1,3 +1,4 @@
+using PCLStorage;
 using Plugin.PCLSpecialFolder.Abstractions;
 using System;
 
@@ -8,5 +9,103 @@ namespace Plugin.PCLSpecialFolder
     /// </summary>
     public class PCLSpecialFolderImplementation : IPCLSpecialFolder
     {
+        /// <summary>
+        /// Application folder
+        /// </summary>
+        public IFolder App
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        /// <summary>
+        /// Local data folder
+        /// </summary>
+        public IFolder Local
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        /// <summary>
+        /// Roaming data folder
+        /// </summary>
+        public IFolder Roaming
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        /// <summary>
+        /// Tamporary data folder
+        /// </summary>
+        public IFolder Temporary
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        /// <summary>
+        /// Cache folder
+        /// </summary>
+        public IFolder Cache
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        /// <summary>
+        /// Documents folder
+        /// </summary>
+        public IFolder Documents
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        /// <summary>
+        /// Pictures folder
+        /// </summary>
+        public IFolder Pictures
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        /// <summary>
+        /// Music folder
+        /// </summary>
+        public IFolder Music
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        /// <summary>
+        /// Videos folder
+        /// </summary>
+        public IFolder Videos
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
     }
 }
