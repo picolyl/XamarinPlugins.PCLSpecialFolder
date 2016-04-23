43c653119e559115b25c201fce7ea8e5c0d813fd
picolyl <picolyl@gmail.com>
2016-04-24 01:45:39 +0900

    Initial projects

---
+++ PCLSpecialFolder/Plugin.PCLSpecialFolder.Abstractions/IPCLSpecialFolder.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Plugin.PCLSpecialFolder.Abstractions
+{
+    /// <summary>
+    /// Interface for PCLSpecialFolder
+    /// </summary>
+    public interface IPCLSpecialFolder
+    {
+    }
+}
---
+++ PCLSpecialFolder/Plugin.PCLSpecialFolder.Desktop/PCLSpecialFolderImplementation.cs
@@ -0,0 +1,12 @@
+using Plugin.PCLSpecialFolder.Abstractions;
+using System;
+
+namespace Plugin.PCLSpecialFolder
+{
+    /// <summary>
+    /// Implementation for Feature
+    /// </summary>
+    public class PCLSpecialFolderImplementation : IPCLSpecialFolder
+    {
+    }
+}
